1b656c85f2184e5467f6ce7c6c43a44579e19a3f
Keila_07 <malebomokhonoana67@gmail.com>
2025-09-09 15:10:50 +0200

    practical task 3

---
+++ age_quiz.py
@@ -0,0 +1,19 @@
+age = int(input("Please enter your age"))
+
+if age > 100:
+ print("Sorry , you're dead")
+
+elif age >= 40:
+ print("You're over the hill")
+
+elif age >= 65:
+ print("Enjoy your retirement")
+
+elif age < 13:
+  print("Your qualify fir the kiddie discount")
+
+elif age == 21:
+  print("Congrats on your 21")
+
+else:
+  print("Age is but a number")
